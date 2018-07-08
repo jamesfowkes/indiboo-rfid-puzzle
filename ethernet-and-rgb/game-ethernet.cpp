ac0e8b34a97b06eb529520abb7077822911ee608
James Fowkes <jamesfowkes@gmail.com>
2018-07-08 12:51:25 +0100

    Change IP and MAC addresses

--- ethernet-and-rgb/game-ethernet.cpp
+++ ethernet-and-rgb/game-ethernet.cpp
@@ -4,8 +4,8 @@
 #include "very-tiny-http.h"
 
 static const uint16_t BUF_SIZE = 256;
-static const uint8_t MAC_ADDRESS[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-static const uint8_t IP_ADDRESS[] = {192, 168, 0, 64};
+static const uint8_t MAC_ADDRESS[] = { 0xCA, 0xFE, 0xE5, 0xCA, 0xBE, 0x01 };
+static const uint8_t IP_ADDRESS[] = {192, 168, 0, 104};
 static const uint8_t GATEWAY[] = {255, 255, 255, 0};
 
 byte Ethernet::buffer[BUF_SIZE];
