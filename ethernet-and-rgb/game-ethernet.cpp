3e4f4d05676ac7e655e51b37fd16145d79370988
James Fowkes <jamesfowkes@gmail.com>
2018-07-08 12:57:10 +0100

    Change MAC address

--- ethernet-and-rgb/game-ethernet.cpp
+++ ethernet-and-rgb/game-ethernet.cpp
@@ -4,7 +4,7 @@
 #include "very-tiny-http.h"
 
 static const uint16_t BUF_SIZE = 256;
-static const uint8_t MAC_ADDRESS[] = { 0xCA, 0xFE, 0xE5, 0xCA, 0xBE, 0x01 };
+static const uint8_t MAC_ADDRESS[] = { 0xCA, 0xFE, 0xE5, 0xCA, 0xBE, 0x04 };
 static const uint8_t IP_ADDRESS[] = {192, 168, 0, 104};
 static const uint8_t GATEWAY[] = {255, 255, 255, 0};
 
